f593119b669621e2dd4d43a315ba27049a42f863
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-11-01 15:09:35 -0300

    message de erro loading e sucess

--- file_supabase/frontend/src/FileUpload/AddAluno.jsx
+++ file_supabase/frontend/src/FileUpload/AddAluno.jsx
@@ -6,24 +6,31 @@ function AddAluno() {
   const [pdfFile, setPdfFile] = useState(null);
   const [nome, setNome] = useState("");
   const [endereco, setEndereco] = useState("");
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
   const handleImageChange = (event) => {
-    setFile(event.target.files[0])
-  }
+    setFile(event.target.files[0]);
+  };
 
   const handlePdfChange = (event) => {
-    setPdfFile(event.target.files[0])
-  }
+    setPdfFile(event.target.files[0]);
+  };
   // console.log(nome, endereco, file, pdfFile)
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // setLoading(true); // Inicia o estado de loading
+    // setError(null); // Limpa qualquer erro anterior
+    // setSuccess(null); // Limpa qualquer mensagem de sucesso anterior
+
     if (!file || !pdfFile) {
       alert("Por favor, insira o arquivo e o imagem.");
       return;
     }
 
-    console.log(nome, endereco, 'antes do append front');
+    console.log(nome, endereco, "antes do append front");
     try {
       const formData = new FormData();
       formData.append("file", file);
@@ -36,11 +43,20 @@ function AddAluno() {
           "Content-Type": "multipart/form-data",
         },
       });
-      console.log(response.data, 'response front');
-      alert(`Upload bem-sucedido: ${response.data.fileUrl}`);
+      console.log(response.data.message, "response front");
+      // alert(`Upload bem-sucedido: ${response.data.fileUrl}`);
+      if (response.status === 200) {
+        setSuccess(response.data.message); // Define mensagem de sucesso
+      }
+      console.log(success)
     } catch (error) {
-      console.error("Erro ao fazer upload do arquivo:", error);
-      alert("Erro ao fazer upload do arquivo.");
+      // console.error("Erro ao fazer upload do arquivo:", error);
+      // alert("Erro ao fazer upload do arquivo.");
+      // Captura o erro do backend
+      setError(error.response?.data?.message || "Erro ao adicionar aluno");
+    } finally {
+      setLoading(false); // Finaliza o loading independente de sucesso ou erro
+      
     }
   };
 
@@ -70,13 +86,14 @@ function AddAluno() {
         </div>
         <div>
           <label htmlFor="file">Selecione uma imagem:</label>
-          <input 
-          type="file" 
-          id="file" 
-          name="file" 
-          accept="image/*" 
-          onChange={handleImageChange}
-          required />
+          <input
+            type="file"
+            id="file"
+            name="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            required
+          />
         </div>
         <div>
           <label htmlFor="pdfFile">Selecione um arquivo:</label>
@@ -85,13 +102,17 @@ function AddAluno() {
             id="pdfFile"
             name="pdfFile"
             accept="application/pdf"
-          onChange={handlePdfChange}
-
+            onChange={handlePdfChange}
             required
           />
         </div>
-        <button type="submit">Enviar</button>
+        <button type="submit" disabled={loading}>
+          {loading ? "Enviando..." : "Adicionar Aluno"}
+        </button>
       </form>
+      {loading && <p>Carregando...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {success && <p style={{ color: "green" }}>{success}</p>}
     </div>
   );
 }
